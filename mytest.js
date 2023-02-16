f955b8b1da329328b3565bcf74b41c54b9c66b30
barna <barnabaskaroly@gmail.com>
2023-02-16 11:10:07 +0100

    good

--- mytest.js
+++ mytest.js
@@ -2,4 +2,5 @@ console.log("something write in js");
 
 var x = "something in x";
 var y = "something in y";
-console.log(x +', '+ y);
+var z = "something in z";
+console.log(x +', '+ y + ", " + z);
